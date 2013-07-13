049fef3ff13ede1b0415a9b6f5a1ac3eb90a4be8
Ibrahim Sha'ath <ibrahimshaath@gmail.com>
2013-07-13 13:19:13 +0100

    Fix GPL notices

--- test/asyncfileobjecttest.cpp
+++ test/asyncfileobjecttest.cpp
@@ -2,20 +2,20 @@
 
   Copyright 2011-2013 Ibrahim Sha'ath
 
-  This file is part of LibKeyFinder.
+  This file is part of KeyFinder.
 
-  LibKeyFinder is free software: you can redistribute it and/or modify
+  KeyFinder is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  LibKeyFinder is distributed in the hope that it will be useful,
+  KeyFinder is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with LibKeyFinder.  If not, see <http://www.gnu.org/licenses/>.
+  along with KeyFinder.  If not, see <http://www.gnu.org/licenses/>.
 
 *************************************************************************/
 
